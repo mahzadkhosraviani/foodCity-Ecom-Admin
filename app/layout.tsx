96a8bf7953becbe37cc65bf58bd5bd002b490c4e
mahzadkhosraviani <khosravianimahzad@gmail.com>
2026-08-02 12:09:54 +0330

    adding the layout of the project

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import NextBprogress from "@/components/libraries/NextNprogress";
 import "./globals.css";
 import BootstrapClient from "@/components/libraries/Bootstrap";
 import Toastify from "@/components/libraries/Toastify";
+import Header from "@/components/layout/Header";
+import SideBar from "@/components/layout/SideBar";
 
 export default function RootLayout({
   children,
@@ -12,7 +14,15 @@ export default function RootLayout({
     <html lang="fa" dir="rtl">
       <body>
         <NextBprogress>
-          {children}
+          <Header />
+          <div className="container-fluid">
+            <div className="row">
+              <SideBar />
+              <main className="col-md-9 ms-sm-auto col-lg-10 px-md-4 mt-4">
+                {children}
+              </main>
+            </div>
+          </div>
           <BootstrapClient />
           <Toastify />
         </NextBprogress>
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
     <>
-      <h1>Admin panel</h1>
+      <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+        <h4 className="fw-bold">داشبورد</h4>
+      </div>
+
+      <div id="chartdiv"></div>
     </>
   );
 }
